25d2139ce3c748cf671c14d3296f63f67584f1eb
piecioshka <piecioshka@gmail.com>
2013-09-06 00:48:06 +0200

    apply mechanism for GitHuB pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+require.config({
+    paths: {
+        'zepto': 'lib/zepto.min',
+        'underscore': 'lib/underscore.min',
+        'events': 'lib/events.min'
+    },
+    shim: {
+        'zepto': {
+            'exports': '$'
+        },
+        'underscore': {
+            'exports': '_'
+        },
+        'events': {
+            'exports': 'Events'
+        }
+    }
+});
+
+require([
+    'zepto',
+    'app'
+],function ($, App) {
+    'use strict';
+
+    function bootstrap(data) {
+        App.MATRIX = data;
+        App.initialize();
+    }
+
+    $.getJSON('config/matrix.json', bootstrap);
+
+});
